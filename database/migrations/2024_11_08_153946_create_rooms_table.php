cc49cb4c87d7f3975fe61aa370c6b3c021d772f9
Ezequiel Oliveira <esofinney@gmail.com>
2024-11-10 22:27:02 -0300

    created room controller and etc

---
+++ database/migrations/2024_11_08_153946_create_rooms_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('hotel_id');
+
+            $table->string('name');
+            $table->string('description');
+
+            $table->foreign('hotel_id')->references('id')->on('hotels');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
